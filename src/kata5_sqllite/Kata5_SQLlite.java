ec47846650fac24002e04c0d75a1dcc638e8bfbe
david <david@DESKTOP-FN26MVG>
2020-11-25 12:14:21 +0000

    v2 created second table

--- src/kata5_sqllite/Kata5_SQLlite.java
+++ src/kata5_sqllite/Kata5_SQLlite.java
@@ -19,12 +19,18 @@ public class Kata5_SQLlite {
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
         
+        String createSql= "CREATE TABLE IF NOT EXISTS direcc_email(id integer PRIMARY KEY AUTOINCREMENT,direccion text NOT NULL);";
+        
         Class.forName("org.sqlite.JDBC");
         
         Connection connection = DriverManager.getConnection("jdbc:sqlite:data/Prueba.db");
         
         try(Statement statement= connection.createStatement()){
+            
+            statement.execute(createSql);
             ResultSet set = statement.executeQuery("SELECT * FROM PEOPLE");
+            
+            
             while (set.next()) {
                 String id = set.getString("id");
                 String name = set.getString("Name");
