4cfa78aa0fcb25d08f5152105b458678071b8ca8
david <david@DESKTOP-FN26MVG>
2020-11-25 12:55:54 +0000

    removed commentaries

--- src/kata5_sqllite/Kata5_SQLlite.java
+++ src/kata5_sqllite/Kata5_SQLlite.java
@@ -1,22 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata5_sqllite;
 
 import java.sql.*;
 
-/**
- *
- * @author david
- */
 public class Kata5_SQLlite {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.ClassNotFoundException
-     */
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
         
         String createSql= "CREATE TABLE IF NOT EXISTS direcc_email(id integer PRIMARY KEY AUTOINCREMENT,direccion text NOT NULL);";
